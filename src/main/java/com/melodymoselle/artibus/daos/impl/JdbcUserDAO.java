b0ca159c6957daa3805b5e564ad9c3c92c1b79e4
Melmo <melodymoselle@gmail.com>
2017-05-16 18:11:10 -0700

    Finished implementation of UserDOA. Still need to test.

--- src/main/java/com/melodymoselle/artibus/daos/impl/JdbcUserDAO.java
+++ src/main/java/com/melodymoselle/artibus/daos/impl/JdbcUserDAO.java
@@ -3,10 +3,7 @@ package com.melodymoselle.artibus.daos.impl;
 import com.melodymoselle.artibus.entities.User;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JdbcUserDAO {
 
@@ -18,13 +15,48 @@ public class JdbcUserDAO {
 
     public User register(String username, String password){
         User user = findUserByUsername(username);
-        if (user == null) {
-            user = new User(username, password);
+        if (user != null) {
+            return null;
         }
+        user = new User(username, password);
+        user = save(user);
+        return user;
     }
 
+    public User findUserById(int id){
+        String sql = "FIND * FROM users WHERE ID = ?";
+        Connection conn = null;
+
+        try {
+            conn = dataSource.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            User user = null;
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                user = new User(
+                        rs.getInt("ID"),
+                        rs.getString("USERNAME"),
+                        rs.getString("PASSWORD")
+                );
+            }
+            rs.close();
+            ps.close();
+            return user;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (conn != null){
+                try {
+                    conn.close();
+                } catch (SQLException e){}
+            }
+        }
+
+    };
+
     public User findUserByUsername(String username){
-        String sql = "FIND * FROM USERS WHERE USERNAME = ?";
+        String sql = "FIND * FROM users WHERE USERNAME = ?";
         Connection conn = null;
 
         try {
@@ -54,4 +86,28 @@ public class JdbcUserDAO {
         }
 
     };
+
+    public User save(User user){
+        String sql = "INSERT INTO users (USERNAME, PASSWORD) VALUES (?, ?)";
+        Connection conn = null;
+
+        try {
+            conn = dataSource.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getUsername());
+            ps.setString(2, user.getPassword());
+            ps.executeUpdate();
+            user.setId(ps.getGeneratedKeys().getInt(1));
+            ps.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {}
+            }
+        }
+        return user;
+    }
 }
